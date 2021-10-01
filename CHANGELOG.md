857ce5862670f4c050b6e61acb1754760b274f9a
Tobias Assmann <tobias.assmann@ecsec.de>
2021-10-01 14:13:56 +0200

    change dep constraints to support TYPO3 11, update CHANGELOG, bump version to 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@
 ### Fixed
 ### Security
 
+## 1.1.0 2021-10-04
+### Added
+- Add support for TYPO3 11
+### Fixed
+- Fix error with TR031303 XML error message
 ## 1.0.0 2021-09-29
 ### Added
 - Initial Release
--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'eID-Login@ecsec.de',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'scheduler' => '10.4.21-10.4.99',
-            'typo3' => '10.4.21-10.4.99',
+            'scheduler' => '10.4.21-11.4.99',
+            'typo3' => '10.4.21-11.4.99',
             'php' => '7.4.8-7.4.99',
         ]
     ],
